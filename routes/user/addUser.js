f9475af5b199559383f919b09158bfddad2fc3ae
Jul <meetredmist@gmail.com>
2020-11-10 10:07:12 +0700

    has password

--- routes/user/addUser.js
+++ routes/user/addUser.js
@@ -2,14 +2,24 @@ const express = require('express')
 const app = express.Router()
 const db = require('../../controller/dbController')
 const mysqlErrorHandler = require('../../controller/midleware/errorMiddleware')
+const { salt } = require('../../helper/bcryptHelper')
 // const uid = require('uid')
 
 app.post('/register', (req, res) => {
-    const body = req.body
-    db.add('users', body)
-        .then(result => {
-            if (result) {
-                res.send('ok')
+    const username = req.body.username
+    const password = req.body.password
+    salt(password)
+        .then(hashedPassword => {
+            const user = {
+                username,
+                password: hashedPassword
+            }
+            db.add('users', user)
+
+        })
+        .then(addUserResult => {
+            if (addUserResult) {
+                res.send(addUserResult)
 
             } else {
                 res.status(400).send('wrong body')
